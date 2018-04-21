a90b7ba8f61860d1b5b56a5f0b8f448f9b2e54cb
TheCodeWorm <4purposeliving@gmail.com>
2018-04-21 15:20:13 -0700

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './header.css';
+
+const Header = () => {
+	return (
+	  <div>
+	  	<ul id="menu">
+        <li>home</li>
+        <li>menu</li>
+        <li>about</li>
+        <li>contact</li>
+      </ul>
+	  </div>
+	);
+}
+
+export default Header;
