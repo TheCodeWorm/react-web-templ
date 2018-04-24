25c704f13831635765a4535337c575cb20ea2228
TheCodeWorm <4purposeliving@gmail.com>
2018-04-24 14:05:20 -0700

    u

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,10 @@
 import React from 'react'
 import './header.css';
 
-onItemClick: function (event) {
-    event.currentTarget.style.backgroundColor = '#ccc';
-},
-
 const Header = () => {
 	return (
 	  <div>
-      <ul className = {'navbar sticky'}>
-        <ul className={'menu-item'} onClick={this.onItemClick}>Contact</ul>
-        <ul className={'menu-item'} onClick={this.onItemClick}>Projects</ul>
-        <ul className={'menu-item'} onClick={this.onItemClick}>Home</ul>
-      </ul>
-    </div>
+  	</div>
 	)
 }
 
--- src/containers/App.js
+++ src/containers/App.js
@@ -5,42 +5,64 @@ import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import Header from '../components/Header';
 import Home_Page from '../components/Home_Page';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
 class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-			robots: [],
-			searchfield: ''
+			//robots: []
+			//, searchfield: ''
+			active_page: 'home'
 		};
+		if (this.active_page === 'projects') {
+			console.log("setting projects..");
+		}
 	}
-
+   
+  /*
 	componentDidMount() {
 		fetch('https://jsonplaceholder.typicode.com/users')
 		.then(response => response.json())
 		.then(users => this.setState({ robots: users }));
 	}
+	*/
+
+	setToProjects = (e) => {
+    e.preventDefault();
+    this.state.active_page = 'projects';
+    console.log(this.state.active_page);
+    this.render();
+  };
 
 	onSearchChange = (event) => {
 		this.setState({ searchfield: event.target.value });
 	}
 
 	render() {
-
-		const { robots, searchfield } = this.state;
+		const { active_page } = this.state;
+		/*
 		const filteredRobots = robots.filter(robot => {
 			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
 		})
+		*/
 		/*
 		if (!robots.length) {
 			return <h1>Loading</h1>
 		} else { 
 			return */ 
 		// use ternary instead of above:
-		return !robots.length ?
-		<h1>Loading</h1> :
+		 return this.state.active_page === '' ?
+		 <h1>Loading...</h1> : 
 			( 
 				<div className='home_page_container'>
+					<nav class="dt w-100 border-box ph5-ns">
+  					<div class="dtc v-mid w-75 tr">
+					    <a class="link dim dark-gray f6 f5-ns dib mr3 mr4-ns" href="#" title="Home">Home</a>
+					    <a class="link dim dark-gray f6 f5-ns dib mr3 mr4-ns" onClick={this.setToProjects} href="#" title="Projects">Projects</a>
+					    <a class="link dim dark-gray f6 f5-ns dib" href="#" title="Contact">Contact</a>
+  					</div>
+					</nav>
 				  <Header />
 				  <Home_Page />
 				  <p>testing</p>
